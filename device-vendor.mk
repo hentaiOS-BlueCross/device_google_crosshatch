e0a84a2a6141f8866217a8e5811e9255940ab9cf
mattDev0 <amatthew642@gmail.com>
2023-05-13 22:42:36 +0100

    remove google assistant prop

--- device-vendor.mk
+++ device-vendor.mk
@@ -28,9 +28,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
-# Google Assistant
-PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
-
 # RCS
 PRODUCT_PACKAGES += \
     PresencePolling \
